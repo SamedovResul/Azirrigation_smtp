1c4b8e6f0a884f1bf4dc5d1d5d4a4e704a931c18
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-04-16 02:07:24 +0400

    modify smtp server for aquaponics-project. create controllers and routes folders. router add routes folder and callback add controllers folder. Done smtp server. but don't test

---
+++ Server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/contactRouter.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("contact/", router);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server start at ${PORT}`));
---
+++ controllers/contactController.js
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer";
+
+const mainEmail = process.env.EMAIL;
+const password = process.env.PASS;
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: true,
+  auth: {
+    user: mainEmail,
+    pass: password,
+  },
+});
+export const contact = (req, res) => {
+  const { fullName, email, message, type } = req.body;
+
+  const mailOptions = {
+    from: mainEmail,
+    to: email,
+    subject: "Client Message",
+    html: `
+    <ul>
+    <li>Client: ${fullName}</li>
+    <li>Type: ${type}</li>
+    <li>Message: ${message}</li>
+    <li>Email: ${email}</li>
+  </ul>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send("Email sent successfully");
+    }
+  });
+};
